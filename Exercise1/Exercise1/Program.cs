308bf253e8e8df28b538a40d54fc98ea5158e75e
Gabrielle <gaboliveirasantana1@gmail.com>
2022-06-16 16:29:31 -0300

    Adicionando três pastas de exercícios de C#

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é ${preco1}");
+            Console.WriteLine($"{produto2}, cujo o preço é ${preco2}");
+            Console.WriteLine($"\nRegistro: {idade} anos de idade, código {codigo} e gênero: {genero}\n");
+
+            Console.WriteLine($"Medida com oito casas deciais: {medida}");
+            Console.WriteLine($"Arredondado(três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
