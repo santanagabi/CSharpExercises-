83cf3a254caa8b58ff126bd461ad3b81bb62aee6
Gabrielle <gaboliveirasantana1@gmail.com>
2022-06-22 22:15:26 -0300

    Inserindo o Desafio  3 do curso do Nélio Alves

---
+++ Challenge3_While/Exercise3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+
+        Console.WriteLine("Insira um número de 1 até 4: ");
+        int tipo = int.Parse(Console.ReadLine());
+
+        while (tipo != 4)
+        {
+            if (tipo == 1)
+            {
+                alcool++;
+            }
+            else if (tipo == 2)
+            {
+                gasolina++;
+            }
+            else if (tipo == 3)
+            {
+                diesel++;
+            }
+
+            tipo = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("MUITO OBRIGADO");
+        Console.WriteLine("Alcool: " + alcool);
+        Console.WriteLine("Gasolina: " + gasolina);
+        Console.WriteLine("Diesel: " + diesel);
+    }
+}
