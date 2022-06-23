c7a738bb728a5db38d4861ca1678069225e721a2
Gabrielle <gaboliveirasantana1@gmail.com>
2022-06-22 22:16:47 -0300

    Adicionando modificações e atualizações do Curso

---
+++ Course/CsharpOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsharpOperators;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool c1 = 2 > 3 || 4 != 5;
+
+        Console.WriteLine(c1);
+    }
+}
---
+++ Course/EstruturaCondicional/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
