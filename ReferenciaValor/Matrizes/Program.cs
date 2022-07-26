cffdf0315e1184e3ec9761b02daa041657593b01
Gabrielle <gaboliveirasantana1@gmail.com>
2022-07-26 01:26:49 -0300

    Atualizando com material de matrizes

---
+++ ReferenciaValor/Matrizes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 linhas e 3 colunas
+            double[,] mat = new double[2, 3];
+
+            // Quantos elementos a matriz possui no total
+            Console.WriteLine(mat.Length);
+
+            // Quantidade de linhas
+            Console.WriteLine(mat.Rank);
+
+            // Dimensão 0 tem 2 linhas
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
