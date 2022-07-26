64caa98c16429afb19f11bd93a1ea4be8ce38b09
Gabrielle <gaboliveirasantana1@gmail.com>
2022-07-25 23:44:52 -0300

    Adicionando atividade de lógica com Csharp

---
+++ LógicaComCSharp/LógicaComCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Switch // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor = "gabi";
+
+            switch (valor)
+            {
+                case "joao":
+                    Console.WriteLine("Não é ela!");
+                    break;
+                case "marcelo":
+                    Console.WriteLine("Não é ela!");
+                    break;
+                case "gabi":
+                    Console.WriteLine("É esta! ");
+                    break;
+                default:
+                    Console.WriteLine("Não achei");
+                    break ;
+            }
+        }
+    }
+}
