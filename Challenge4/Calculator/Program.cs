175bf09a1f8e8c9ad7dca47a6b897e66274b452f
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 19:14:29 -0300

    Criando saida da calculadora

--- Challenge4/Calculator/Program.cs
+++ Challenge4/Calculator/Program.cs
@@ -18,6 +18,7 @@ namespace Calculator // Note: actual namespace depends on the project name.
       Console.WriteLine("2 - Subtração");
       Console.WriteLine("3 - Divisão");
       Console.WriteLine("4 - Multiplicação");
+      Console.WriteLine("5 - Sair");
 
       Console.WriteLine("-----------------------");
       Console.WriteLine("Selecione uma opção: ");
@@ -30,6 +31,7 @@ namespace Calculator // Note: actual namespace depends on the project name.
         case 2: Subtracao(); break;
         case 3: Divisao(); break;
         case 4: Multiplicacao(); break;
+        case 5: System.Environment.Exit(0); break;
         default: Menu(); break;
       }
     }
@@ -50,7 +52,7 @@ namespace Calculator // Note: actual namespace depends on the project name.
       // Console.WriteLine($"O resultado da soma é {v1 + v2}");
 
       Console.ReadKey();
-
+      Menu();
     }
 
     static void Subtracao()
@@ -68,6 +70,8 @@ namespace Calculator // Note: actual namespace depends on the project name.
       float resultado = v1 - v2;
       Console.WriteLine($"O resultado da subtração é {resultado}");
       Console.ReadKey();
+      Menu();
+
     }
 
     static void Divisao()
@@ -85,6 +89,8 @@ namespace Calculator // Note: actual namespace depends on the project name.
       float resultado = v1 / v2;
       Console.WriteLine($"O resultado da divisão é: {resultado}");
       Console.ReadKey();
+      Menu();
+
     }
 
     static void Multiplicacao()
@@ -103,6 +109,8 @@ namespace Calculator // Note: actual namespace depends on the project name.
       Console.WriteLine($"O resultado da multiplicação é {resultado}");
 
       Console.ReadKey();
+      Menu();
+
     }
   }
 }
