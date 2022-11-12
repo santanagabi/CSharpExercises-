b7617d2f2e103e4758c6152f1e81eb439e789a72
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 18:51:33 -0300

    método de subtração

--- Challenge4/Calculator/Program.cs
+++ Challenge4/Calculator/Program.cs
@@ -7,6 +7,7 @@ namespace Calculator // Note: actual namespace depends on the project name.
     static void Main(string[] args)
     {
       Soma();
+      Subtracao();
     }
 
     static void Soma()
@@ -23,6 +24,26 @@ namespace Calculator // Note: actual namespace depends on the project name.
       // Console.WriteLine(" O resultado da soma é: " + resultado);
       Console.WriteLine($"O resultado da soma é {resultado}");
       // Console.WriteLine($"O resultado da soma é {v1 + v2}");
+
+      Console.ReadKey();
+
+    }
+
+    static void Subtracao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor:");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Primeiro valor:");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 - v2;
+      Console.WriteLine($"O resultado da subtração é {resultado}");
+      Console.ReadKey();
     }
   }
 }
