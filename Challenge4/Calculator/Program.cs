070bd28a555cdb21e3c1d20385dbdee013427316
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 18:56:17 -0300

    Método de divisão

--- Challenge4/Calculator/Program.cs
+++ Challenge4/Calculator/Program.cs
@@ -6,8 +6,7 @@ namespace Calculator // Note: actual namespace depends on the project name.
   {
     static void Main(string[] args)
     {
-      Soma();
-      Subtracao();
+      Divisao();
     }
 
     static void Soma()
@@ -43,6 +42,23 @@ namespace Calculator // Note: actual namespace depends on the project name.
 
       float resultado = v1 - v2;
       Console.WriteLine($"O resultado da subtração é {resultado}");
+      Console.ReadKey();
+    }
+
+    static void Divisao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 / v2;
+      Console.WriteLine($"O resultado da divisão é: {resultado}");
       Console.ReadKey();
     }
   }
