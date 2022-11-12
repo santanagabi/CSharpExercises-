630b50d6bb9ea33e46b59a91d40685a7a3d02aa4
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 17:57:43 -0300

    estudando structs

--- Stucts/Program.cs
+++ Stucts/Program.cs
@@ -6,11 +6,24 @@ namespace Structs // Note: actual namespace depends on the project name.
   {
     static void Main(string[] args)
     {
+      Product mouse = new Product(1, "Mouse gamer", 299.97);
+
+      mouse.Id = 55;
+
+      Console.WriteLine(mouse.Id);
+      Console.WriteLine(mouse.Name);
+      Console.WriteLine(mouse.Price);
 
     }
   }
   struct Product
   {
+    public Product(int id, string name, double price)
+    {
+      Id = id;
+      Name = name;
+      Price = price;
+    }
     public int Id;
     public string Name;
     public double Price;
