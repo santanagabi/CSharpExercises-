2f43f6a4512d4962f71ff57c88ca12e7c6159c64
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 19:00:53 -0300

    Método de multiplicação

--- Challenge4/Calculator/Program.cs
+++ Challenge4/Calculator/Program.cs
@@ -61,5 +61,23 @@ namespace Calculator // Note: actual namespace depends on the project name.
       Console.WriteLine($"O resultado da divisão é: {resultado}");
       Console.ReadKey();
     }
+
+    static void Multiplicacao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 * v2;
+      Console.WriteLine($"O resultado da multiplicação é {resultado}");
+
+      Console.ReadKey();
+    }
   }
 }
