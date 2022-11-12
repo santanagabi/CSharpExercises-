20e4cd587abb2af8be95a2e3d18585fc9e8269bd
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 18:27:49 -0300

    Projeto calculadora csharp

--- Challenge4/Calculator/Program.cs
+++ Challenge4/Calculator/Program.cs
@@ -9,6 +9,12 @@ namespace Calculator // Note: actual namespace depends on the project name.
       Console.Clear();
       Console.WriteLine("Primeiro valor: ");
       float v1 = float.Parse(Console.ReadLine());
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      Console.WriteLine(" O resultado da soma é: ");
 
       Console.WriteLine(v1);
     }
