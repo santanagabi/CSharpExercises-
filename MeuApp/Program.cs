a4b43249f59feab902b9b211df447a1b43525c0d
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-12 17:51:11 -0300

    estudando structs

---
+++ MeuApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste // Note: actual namespace depends on the project name.
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Stucts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Structs // Note: actual namespace depends on the project name.
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+    }
+  }
+  struct Product
+  {
+
+  }
+}
