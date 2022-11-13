b5a4d6e6e55f87bc13607de94932abb588d009fd
santanagabi <gaboliveirasantana1@gmail.com>
2022-11-13 15:54:16 -0300

    Estudando classes e objetos com C#

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassesAndObjects // Note: actual namespace depends on the project name.
+{
+  public class Person
+  {
+    public string Name;
+    public int Age;
+    public bool HasPet;
+
+    public void Greeting()
+    {
+      Console.WriteLine("Hi! My name is " + Name + " and my age is " + Age);
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person person = new Person();
+
+      person.Name = "John";
+      person.Age = 28;
+      person.HasPet = true;
+
+      person.Greeting();
+    }
+  }
+}
