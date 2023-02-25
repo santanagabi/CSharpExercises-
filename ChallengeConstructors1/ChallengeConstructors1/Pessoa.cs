71bb18ae0639ad350a5c2db5797e866417afe47c
santanagabi <gaboiveirasantana1@gmail.com>
2023-02-25 18:52:32 -0300

    learning constructors in c#

--- ChallengeConstructors1/ChallengeConstructors1/Pessoa.cs
+++ ChallengeConstructors1/ChallengeConstructors1/Pessoa.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace ChallengeConstructors1
 {
-    internal class Pessoa
+    class Pessoa
     {
+        public string Nome;
+
+        public string Endereco;
+
+        public string Telefone;
+
+        // Construtor default
+        public Pessoa() { }
+
+        // Construtor
+        public Pessoa(string nome, string endereco, string telefone)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+
+        public string primeiroNome()
+        {
+            return Nome;
+        }
     }
 }
--- ChallengeConstructors1/ChallengeConstructors1/Program.cs
+++ ChallengeConstructors1/ChallengeConstructors1/Program.cs
@@ -2,11 +2,27 @@
 
 namespace ChallengeConstructors1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Pessoa maria = new Pessoa();
+            maria.Nome = "Maria Alice";
+            maria.Endereco = "Goiania";
+            maria.Telefone = "(XX) 0000-000";
+
+            Pessoa jonas = new Pessoa("Jonas", "Feira de Santana", "(99)999-9999");
+
+            Console.WriteLine("--------- Primeira Pessoa -----------");
+            Console.WriteLine("Nome completo " + maria.Nome);
+            Console.WriteLine("Seu endereço é: " + maria.Endereco);
+            Console.WriteLine("Seu telefone é: " + maria.Telefone);
+
+            Console.WriteLine("--------- Segunda Pessoa ----------");
+            Console.WriteLine("Nome completo: " + jonas.Nome);
+            Console.WriteLine("Seu endereço é: " + jonas.Endereco);
+            Console.WriteLine("Seu telefone é: " + jonas.Telefone);
         }
     }
 }
