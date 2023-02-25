5fef21bd302d1716ca9e601fec1b2ca567e683da
Gabrielle <gaboliveirasantana1@gmail.com>
2023-02-25 19:30:04 -0300

    Learning about Static Members

---
+++ StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double volume = Calculadora.Volume(raio);
+
+            double circ = Calculadora.Circunferencia(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
