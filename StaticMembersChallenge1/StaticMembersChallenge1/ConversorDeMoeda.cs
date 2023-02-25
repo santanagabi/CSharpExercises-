942d2c6511aa53e229c02a12b6d1dee8a2c00714
Gabrielle <gaboliveirasantana1@gmail.com>
2023-02-25 20:15:29 -0300

    Challenge about static members

---
+++ StaticMembersChallenge1/StaticMembersChallenge1/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace CurrencyConverter
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double dolar, double cotacao)
+        {
+            double total = dolar * cotacao;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ StaticMembersChallenge1/StaticMembersChallenge1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace CurrencyConverter;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* Faça um programa para ler a cotação do dólar, e depois um valor em dólares a 
+         * ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai pagar
+         * pelos dólares, considerando ainda que a pessoa terá que pagar 6 % de IOF sobre 
+         * o valor em dólar. Criar uma classe ConversorDeMoeda para ser responsável pelos 
+         * cálculos.
+        */
+
+        Console.WriteLine("Qual é sua cotação em dólar?");
+        double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Quantos dólares você vai comprar?");
+        double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double valorPago = ConversorDeMoeda.DolarParaReal(cotacao,dolar);
+
+       Console.WriteLine("Valor a ser pago em reais = " + valorPago.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
