f11735cff693ab247db3e6afde3d5bbcdea0b9f2
santanagabi <gaboiveirasantana1@gmail.com>
2023-02-25 16:49:50 -0300

    learning constructors in c#

---
+++ Construtor/Construtor/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+namespace Construtor
+{
+    class Produto
+    {
+        /* Atributos */
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        /* Método */
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
