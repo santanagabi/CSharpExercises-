329f4826d6b8edaf704eaffbef6d766416d1f717
Gabrielle <gaboliveirasantana1@gmail.com>
2023-02-26 21:15:02 -0300

    studying about constructor

--- Construtor/Construtor/Produto.cs
+++ Construtor/Construtor/Produto.cs
@@ -7,8 +7,8 @@ namespace Construtor
         // Encapsulamento = não possa expor atributo ou operação que possa prejudicar
         // a integridade do meu produto
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         // Construtor padrão
         public Produto()
@@ -18,8 +18,8 @@ namespace Construtor
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         // Propeties / Propiedades
@@ -33,36 +33,26 @@ namespace Construtor
             }
         }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
-
-        public int Quantidade
-        {
-            get { return _quantidade; }
-        }
-
         /* Método */
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade+= quantidade;
         }
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
         public override string ToString()
         {
             return _nome
             + ", $ "
-            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + _quantidade
+            + Quantidade
             + " unidades, Total: $ "
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
