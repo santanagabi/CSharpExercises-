9275042aae5fe72d5d9c835ad46a75cc3b17925e
santanagabi <gaboliveirasantana1@gmail.com>
2023-04-23 21:27:45 -0300

    Learning about MVC and Model with .net5

---
+++ ORM_Entity_Framework/ORM_Entity_Framework/Migrations/20230423202729_ormdb.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ORM_Entity_Framework.Migrations
+{
+    public partial class ormdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genero",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genero", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filme",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(nullable: true),
+                    GeneroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filme", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Filme_Genero_GeneroId",
+                        column: x => x.GeneroId,
+                        principalTable: "Genero",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filme_GeneroId",
+                table: "Filme",
+                column: "GeneroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filme");
+
+            migrationBuilder.DropTable(
+                name: "Genero");
+        }
+    }
+}
---
+++ ORM_Entity_Framework/ORM_Entity_Framework/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ORM_Entity_Framework
+{
+    public class Genero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        // Cada genero pode estar associado a 1 ou mais filmes
+        public ICollection<Filme> Filme { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+
+        // Configuração do Entity Framework -> relacionamentos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Qual o banco de dados vamos usar
+            // Server=.\MSSQLSERVER1 -> usando o meu
+            optionsBuilder.UseSqlServer(@"Server=.\MSSQLSERVER1;Database=orm;Trusted_Connection=True;");
+        }
+    }
+
+    public class Filme
+    {
+        // Key = chave e auto-incrementa
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int GeneroId { get; set; }
+
+        [ForeignKey("GeneroId")]
+
+        public Genero Genero { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ApplicationContext())
+            {
+                // Adicionando novo genero
+                var genero = new Genero()
+                {
+                    Descricao = "Fantasia"
+                };
+                context.Genero.Add(genero);
+                context.SaveChanges();
+
+                // Adicionando novo filme
+                var filme = new Filme()
+                {
+                    Titulo = "Harry Potter",
+                    GeneroId = genero.Id
+                };
+                context.Filme.Add(filme);
+                context.SaveChanges();
+            }
+        }
+    }
+}
