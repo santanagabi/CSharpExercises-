ab643be077f75fd63fbb34dc6cfac938ed141ed8
santanagabi <gaboliveirasantana1@gmail.com>
2023-04-30 18:01:38 -0300

    Utilizando o Entity Framework

---
+++ app-web-backend/app-web-backend/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // passa do startup as configurações e coloca no contexto da aplicação
+        // configuração do entity framework
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
