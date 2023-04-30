adde0f342474dbf49607c838ba60a4d2898d60a5
santanagabi <gaboliveirasantana1@gmail.com>
2023-04-30 18:19:07 -0300

    feature: Configuração do Entity Framework

--- app-web-backend/app-web-backend/Models/ApplicationDbContext.cs
+++ app-web-backend/app-web-backend/Models/ApplicationDbContext.cs
@@ -12,6 +12,15 @@ namespace app_web_backend.Models
         // configuração do entity framework
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            // criar o banco de dados
+            // ferramentas -> gerenciador de pacotes -> console gerenciador de pacotes
+            // comandos migrations
+            // 1 - add-migration "Nome da Migration"
+            // 2 - update-database
         }
+
+        // criar veiculos e configurando a tabela no banco de dados
+        // coleção de veiculos
+        public DbSet<Veiculo> Veiculos { get; set; }
     }
 }
