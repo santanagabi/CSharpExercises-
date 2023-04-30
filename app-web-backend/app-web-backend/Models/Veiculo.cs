15f94fb214b90bfbb54afd38f94f390f4a553675
santanagabi <gaboliveirasantana1@gmail.com>
2023-04-30 17:21:39 -0300

    Feature: Adiciona o model Veiculo

---
+++ app-web-backend/app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Placa é obrigatória"!)]
+        public string Placa { get; set; }
+    }
+}
