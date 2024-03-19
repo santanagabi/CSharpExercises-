208e8af74001f19ada3cefc49cf959b2523e4b5b
santanagabi <gaboliveirasantana1@gmail.com>
2024-03-18 22:11:04 -0300

    Criando serviço Books Service e criando um CRUD

--- APIBookStore/APIBookStore/Program.cs
+++ APIBookStore/APIBookStore/Program.cs
@@ -1,4 +1,5 @@
 using APIBookStore.Models;
+using APIBookStore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Instancia de configuração e configurações do banco de dados
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
+
+// Adicionar um serviço chamado BooksService, criando um serviço singleton
+builder.Services.AddSingleton<BooksService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ APIBookStore/APIBookStore/Services/BooksService.cs
@@ -0,0 +1,55 @@
+﻿using APIBookStore.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
+namespace APIBookStore.Services
+{
+    public class BooksService
+    {
+        // Criando nosso serviço
+        // Serviço de conexão
+
+        // Chamar a collection do MongoDB
+        private readonly IMongoCollection<Book> _booksCollection;
+        public BooksService(
+            IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+        {
+            // Responsável por se conectar ao servidor MongoDB
+            var mongoClient = new MongoClient(
+                bookStoreDatabaseSettings.Value.ConnectionString);
+
+            // Referencia o nome do banco de dados
+            var mongoDatabase = mongoClient.GetDatabase(
+                bookStoreDatabaseSettings.Value.DatabaseName);
+
+            // Referência á coleção de livros no banco de dados
+            // _booksCollection será usado posteriormente para interagir com os documentos na coleção de livros
+            _booksCollection = mongoDatabase.GetCollection<Book>(
+                bookStoreDatabaseSettings.Value.BooksCollectionName);
+        }
+
+        // Get - geral
+        // Procurar todos os itens da lista
+        public async Task<List<Book>>GetBooksAsync() => 
+            await _booksCollection.Find(_=> true).ToListAsync();
+
+        // Get - por Id
+        public async Task<Book?>GetAsync(string id) =>
+            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        // Create async
+        // Método é responsável por adicionar um novo livro na coleção
+        public async Task CreateAsync(Book newBook) => 
+            await _booksCollection.InsertOneAsync(newBook);
+
+        // Update
+        // Esse método é usado para atualizar um livro existente com base no Id
+        public async Task UpdateAsync(string id, Book updatedBook) =>
+            await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+        // Delete
+        // Exclui um livro da coleção com base no I
+        public async Task RemoveAsync(string id) =>
+            await _booksCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
