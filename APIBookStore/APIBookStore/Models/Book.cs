90446ef14f94e62c47a7128206320b16ad9161bd
santanagabi <gaboliveirasantana1@gmail.com>
2024-03-18 21:25:35 -0300

    Inserindo API Books, estudo da PUC

---
+++ APIBookStore/APIBookStore/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIBookStore.Models
+{
+    public class Book
+    {
+        // Classe de livros -> mapeia objeto -> link para coleção do MongoDB
+        // BSonID -> designar como chave primária do nosso documento
+
+        // Criar algumas variaveis
+        // Id será usado como String e podera anular (em banco relacional não pode)
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = null!;
+        public string Author { get; set; } = null!;
+    }
+}
---
+++ APIBookStore/APIBookStore/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace APIBookStore.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        // Criado classe para armazenar essas configurações
+        // Adicionar esse serciço no Program.cs
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
